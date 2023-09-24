b9e935ac83665e0a5495c546953c41a62fba5502
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-09-24 17:23:51 +0300

    a

--- scripts/index.js
+++ scripts/index.js
@@ -71,8 +71,92 @@ let circle = ()=>{
     }
 }
 
-let citySpeed = ()=>(
-    distance = prompt("Введіть відстань між двома містами: ")
-    time = prompt("Введіть час за який ви хочете дістатися другого міста: ")
+let citySpeed = ()=>{
+    let distance = prompt("Введіть відстань між двома містами (У км): ")
+    let time = prompt("Введіть час за який ви хочете дістатися другого міста: ")
+
+    let speed = distance / time
+    if(distance <= 0 || time <= 0){
+        alert("Число повинне бути більше від 0 і не дорівнювати 0")
+        citySpeed()
+    }else if (isNaN(speed)){
+        alert("Ви повинні ввести число, а не цифру")
+        citySpeed()
+    }else{
+        alert(`${speed}км/год`)
+    }
     
-)
+}
+
+let converter = ()=>{
+    let dollar = prompt("Введіть суму доларів: ")
+
+    const RATE = 0.9128
+
+    let sum = dollar * RATE
+
+    if(dollar <= 0){
+        alert("Число повинне бути більше від 0 і не дорівнювати 0")
+        converter()
+    }else if (isNaN(sum)){
+        alert("Ви повинні ввести число, а не цифру")
+        converter()
+    }else{
+        alert(`${sum.toFixed(2)} Євро`)
+
+        return sum
+    }
+}
+
+let usbdrive = ()=>{
+    let size = prompt("Введіть обсяг флешки (у ГБ): ")
+
+    let count = size / 0.82
+
+    if(size <= 0){
+        alert("Число повинне бути більше від 0 і не дорівнювати 0")
+        usbdrive()
+    }else if (isNaN(count)){
+        alert("Ви повинні ввести число, а не цифру")
+        usbdrive()
+    }else{
+        alert(`${Math.round(count)} файлів`)
+
+        return count
+    }
+}
+
+let chocolate = ()=>{
+    let money = prompt("Введіть суму грощей: ")
+    let cost = prompt("Введіть ціну однієї шоколадки: ")
+
+    let count =  money / cost
+    let remainder = money - (cost * Math.round(count))
+
+    if(money <= 0 || cost <= 0){
+        alert("Число повинне бути більше від 0 і не дорівнювати 0")
+        chocolate()
+    }else if (isNaN(count) || isNaN(remainder)){
+        alert("Ви повинні ввести число, а не цифру")
+        chocolate()
+    }else if (remainder == 0){
+        alert(`Ви зможете купити ${Math.round(count)} шоколадок без здачі`)
+        return count, remainder
+    }else{
+        alert(`Ви зможете купити ${Math.round(count)} шоколадок і здача ${remainder}`)
+
+        return count, remainder
+    }
+}
+    
+let oddeven = ()=>{
+    let number = prompt("Введіть ціле число: ")
+
+    let even = number % 2 == 0
+
+    let result = even ? "Парне" : "Непарне"
+
+    alert(result)
+}
+
+oddeven()
