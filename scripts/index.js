77189224ad47564c56b1f40ebc1e5f7677151aa0
kurulo kezemiakin <pipipupi.kom@gmail.com>
2023-10-23 14:57:58 +0300

    a

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     BURGER_BTN.addEventListener("click", ()=>{
     BURGER_BTN.classList.toggle('active')
     
-    // const MOB_MENU = document.querySelector(".mobile_menu")
-    // MOB_MENU.classList.toggle('active')
+    const MOB_MENU = document.querySelector(".headerContainer__headerMenu_mobileNav")
+    MOB_MENU.classList.toggle('active')
 
     const BODY = document.querySelector(".body")
     BODY.classList.toggle('fixed')
